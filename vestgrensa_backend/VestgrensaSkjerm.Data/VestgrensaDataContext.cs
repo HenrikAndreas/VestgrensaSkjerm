502bd44f36b8a95f25e20d703ee054576edf473d
Henrik Andreas <henrikbratli@live.no>
2022-07-27 15:03:16 +0200

    removing bin & obj folders

--- vestgrensa_backend/VestgrensaSkjerm.Data/VestgrensaDataContext.cs
+++ vestgrensa_backend/VestgrensaSkjerm.Data/VestgrensaDataContext.cs
@@ -13,12 +13,4 @@ public class VestgrensaDataContext : DbContext
         
     }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Resident>()
-            .Property(resident => resident.Current)
-            .HasDefaultValue(true);
-    }   
 }
