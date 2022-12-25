14d19022d09526c9255c09d3384a6b66e343b9d8
HenrikAndreas <henrikbratli@live.no>
2022-12-25 17:34:42 +0100

    fixed environment variables

--- vestgrensa_frontend/src/api/api.tsx
+++ vestgrensa_frontend/src/api/api.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-const vestgrensaApi: string = `https://localhost:7176`;
+const vestgrensaApi: string = process.env.REACT_APP_API_URL!!;
 
 const api: AxiosInstance = axios.create({
     baseURL: vestgrensaApi,
